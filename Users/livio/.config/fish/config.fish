4770322d52253f476ccd9d1aa8f48aff6ce4b3e0
Livio Bieri <livio@livio.li>
2016-01-04 11:13:30 +0100

    Several changes in different areas mainly fish

--- Users/livio/.config/fish/config.fish
+++ Users/livio/.config/fish/config.fish
@@ -12,7 +12,7 @@ end
 # using Base16-Shell thus we need to do this in order to
 # to have a working color scheme for more information see:
 # https://github.com/chriskempson/base16-shell
-eval sh $HOME/.config/base16-shell/base16-default.dark.sh
+eval sh $HOME/.config/base16-shell/base16-eighties.dark.sh
 
 # colorize grep
 alias grep "grep -n --color"
@@ -31,7 +31,57 @@ alias sl "ls" # :-/
 # helper alias for SyncMe
 alias fhs "pushd .; cd ~/Dropbox/FHNW/SyncMe/SyncMe/; make -i | less; popd; clear"
 
+# no greeting
+set fish_greeting ""
+
 # run fish in vim mode
 fish_vi_mode
+
+function fish_mode_prompt --description "Displays the current mode"
+  # Do nothing if not in vi mode
+  if set -q __fish_vi_mode
+    switch $fish_bind_mode
+      case default
+        set_color --bold --background red white
+        echo ' N '
+      case insert
+        set_color --bold --background blue white
+        echo ' I '
+      case replace-one
+        set_color --bold --background green white
+        echo ' R '
+      case visual
+        set_color --bold --background magenta white
+        echo ' V '
+    end
+    set_color normal
+    echo -n ' '
+  end
+end
+
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+  end
+
+  # PWD
+  set_color blue
+  echo -n (prompt_pwd)
+
+  set_color green
+  printf '%s ' (__fish_git_prompt)
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  set_color red
+  echo -n '>> '
+
+  set_color normal
+end
+
 
 source ~/.config/fish/fishmarks.fish
--- Users/livio/.vimrc
+++ Users/livio/.vimrc
@@ -62,7 +62,7 @@ vmap <C-v> <Plug>(expand_region_shrink)
 let base16colorspace=256 " Access colors present in 256 colorspace
 set t_Co=256
 set background=dark
-colorscheme base16-default
+colorscheme base16-eighties
 set backspace=indent,eol,start
 set mouse=a " use mouse :)
 set langmenu=en_US.UTF-8
@@ -76,7 +76,7 @@ syntax on
 
 " highlight current line
 set cursorline
-hi CursorLineNr cterm=Bold ctermfg=LightRed
+hi CursorLineNr cterm=Bold ctermfg=Blue
 
 " no need for it ~
 set nobackup
