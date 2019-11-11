541d648fabf77e851383cfc67ac55e523befa36c
Livio Bieri <livio@livio.li>
2019-11-11 12:30:07 +0800

    git: fix github.com/so-fancy/diff-so-fancy/issues/296

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,11 +7,11 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 
 # Set a faster key repeat
-defaults write NSGlobalDomain InitialKeyRepeat 15
-defaults write NSGlobalDomain KeyRepeat 2
+defaults write NSGlobalDomain InitialKeyRepeat 10
+defaults write NSGlobalDomain KeyRepeat 1
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false 
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
 # Show Safari Favorites Bar
 defaults write com.apple.Safari ShowFavoritesBar -bool true
